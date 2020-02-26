6b8ddbc88cd2b15c988a7a9854c16743255eccab
Suhaib Habibullah <suhaib.habibullah@gmail.com>
2020-02-25 23:20:25 -0500

    Add attributes for columns and rows

--- connect.js
+++ connect.js
@@ -3,8 +3,8 @@ class Connect4 {
 		this.ROWS = 6;
 		this.COLS = 7;
 		this.selector = selector
-
 		this.createGrid();
+		this.setupEventListners();
 	}
 
 	createGrid() {
@@ -15,11 +15,20 @@ class Connect4 {
 			// this loop results in 7 div#col being appended to the newly created $row element
 			for (let col = 0; col < this.COLS; col++) {
 				const $col = $('<div>')
-					.addClass('col empty');
+					.addClass('col empty')
+					.attr('data-col', col)
+					.attr('data-row', row);
 					$row.append($col);
 			}
 			$board.append($row); // this adds 1 $row element which now has 7 $col in it to the board
 		}
 		console.log($board.html());
+	}
+
+	setupEventListners() {
+		const $board = $(this.selector);
+		$board.on('mouseenter', '.col.empty', function(){
+			console.log('here', this);
+		})
 	}
 }
